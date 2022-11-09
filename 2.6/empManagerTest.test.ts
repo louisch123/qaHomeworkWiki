ed4c4d5fc3e0a729df85a121454dce871d9fb8a2
louischarleshaynes <louisch123@yahoo.com>
2022-11-09 15:52:57 -0800

    2.6.2 Homework

--- 2.6/empManagerTest.test.ts
+++ 2.6/empManagerTest.test.ts
@@ -1,44 +1,52 @@
-import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
+import {Builder,By, Capabilities, until, WebDriver, WebElement} from "selenium-webdriver";
   const chromedriver = require("chromedriver");
 
   const driver: WebDriver = new Builder()
   .withCapabilities(Capabilities.chrome())
   .build();
 
-  class employeePage {
+  export class employeePage {
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
         //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
+        constructor(driver:WebDriver, url: string){
+            this.driver = driver
+            this.url = url
+        }
   }
 
-  describe("Employee Manger Test", () => {
+  describe("Employee Manager Test", () => {
       beforeEach(async () => {
-          await driver.getemployeePage.navigate();
-      })
+          await employeePage.navigate("");// Are we giving navigate an attribute
+
+      });
       afterAll(async () => {
           await driver.quit()
-      })
-      const header: By = By.name()
+      });
+      /*Locators Syntax: CSS-Static> $$('. or #attribute') or $$('[name = "attribute"]') XParth> $x('//li[@name="attribute*]')*/
+      
+      const header: By = By.id("titleText") // not able to find this selector // 
       const addEmployee: By = By.name("inputField");
-      const newEmployee: By = By.()
-      const nameInput: By = By.
-      const phoneInput: By = By.
-      const titleInput: By = By.
+      const newEmployee: By = By.name("employee11");
+      const nameInput: By = By.name("materialInput");
+      const phoneInput: By = By.name("materialInput");
+      const titleInput: By = By.name("materialInput"); // Would this qualify for By.css? How do I determine in the elements tool on the Console?
     
       
       
       
       test("adding an employee", async () => {
-          await driver.wait(until.elementLocated(By.name("header"))
-          await driver.findElement(By.name("addEmployee").click()
-          await driver.findElement(By.name("newEmployee").click()
-          await driver.findElement(emPage.nameInput).click()
-          await driver.findElement(emPage.nameInput).clear()
-          await driver.findElement(emPage.nameInput).sendKeys("Change this")
-          await driver.findElement(emPage.phoneInput).click()
-          await driver.findElement(emPage.phoneInput).clear()
-          await driver.findElement(emPage.phoneInput).sendKeys("Change this")
-          await driver.findElement(emPage.titleInput).click()
-          await driver.findElement(emPage.titleInput).clear()
-          await driver.findElement(emPage.titleInput).sendKeys("Change this")
-  })
+          await driver.wait(until.elementLocated(By.id("header")))
+          await driver.findElement(By.name("addEmployee")).click()
+          await driver.findElement(By.name("newEmployee")).click()
+          await driver.findElement(By.name("employee11")).click()
+          await driver.findElement(By.name ("nameEntry")).clear()
+          await driver.findElement(By.name("materialInput")).sendKeys("John Cusayk")
+          await driver.findElement(By.name("materialInput")).click()
+          await driver.findElement(By.name("materialInput")).clear()
+          await driver.findElement(By.name("materialInput")).sendKeys("1234567890")
+          await driver.findElement(By.name("materialInput")).click()
+          await driver.findElement(By.name("materialInput")).clear()
+          await driver.findElement(By.name("materialInput")).sendKeys("Motivational Speaker")
+      });
+  });
